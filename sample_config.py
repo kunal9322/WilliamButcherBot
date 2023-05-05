bc6712897c8d2cac910037578e5ab2e552718068
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-05-05 08:59:34 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN = os.environ.get('BOT_TOKEN','6221284663:AAGop_cKtr03r4Tv8k7NeDpV4lb0X1uLGck')
 API_ID = 16743442'))
 SESSION_STRING = os.environ.get('SESSION_STRING', '')
 API_HASH = os.environ.get('API_HASH','12bbd720f4097ba7713c5e40a11dfd2a')
